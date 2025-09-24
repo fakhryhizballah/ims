a37c37ec271349d3fbff3bfb68fdb6d316fe8d3b
fakhryhizballah <falehry88@gmail.com>
2025-09-24 23:05:42 +0700

    seeder saruan

---
+++ seeders/20250924041747-seed-jenis-satuan.js
@@ -0,0 +1,63 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    return queryInterface.bulkInsert('JenisSatuans', [
+      {
+        slug: 'pcs',
+        nama_satuan: 'Pcs',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        slug: 'box',
+        nama_satuan: 'Box',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        slug: 'pack',
+        nama_satuan: 'Pack',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        slug: 'bks',
+        nama_satuan: 'Kilogram',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        slug: 'kg',
+        nama_satuan: 'Kilogram',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        slug: 'ltr',
+        nama_satuan: 'Liter',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
